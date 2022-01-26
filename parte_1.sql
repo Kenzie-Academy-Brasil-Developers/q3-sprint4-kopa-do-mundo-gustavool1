e2bb282ec20136bdefbe507b46acf4bca74641a5
Gustavo <go8895806@gmail.com>
2022-01-26 15:31:11 -0300

    feat:finish the part 1

---
+++ parte_1.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS kopas(
+	id BIGSERIAL CONSTRAINT pk_kopas PRIMARY KEY,
+	selecao VARCHAR(128) NOT NULL UNIQUE ,
+	qnt_copas INTEGER
+)
+
+INSERT INTO 
+	kopas(selecao, qnt_copas)
+VALUES
+('Brasil', 5),
+('Alemanha', 5),
+( 'Itália', 4),
+('Argentina', 2),
+('França', 2),
+('Uruguai', 2),
+('Inglaterra', 1),
+('Espanha', 1),
+('Japão', 1);
+
+
+UPDATE kopas SET qnt_copas = 4 WHERE selecao = 'Alemanha'  RETURNING * ;
+DELETE FROM kopas WHERE selecao = 'Japão' RETURNING * ;
+SELECT * FROM kopas ; 
